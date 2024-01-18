9d5f03237ae412ff9a5c221487156728d58c8a47
M1chael910 <mpmurphybusiness@gmail.com>
2024-01-17 19:38:51 -0600

    Finished basic calculator

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function Add() {}
-function Subtract() {}
-function Multiply() {}
-function Divide() {}
+function Calculate(operation) {
+  const firstBox = document.getElementById("first-box");
+  const secondBox = document.getElementById("second-box");
+  const resultLabel = document.getElementById("results-label");
+  let x = Number(firstBox.value);
+  let y = Number(secondBox.value);
+  if (operation == 1) {
+    resultLabel.innerHTML = x + y;
+  } else if (operation == 2) {
+    resultLabel.innerHTML = x - y;
+  } else if (operation == 3) {
+    resultLabel.innerHTML = x * y;
+  } else if (operation == 4) {
+    resultLabel.innerHTML = x / y;
+  }
+}
