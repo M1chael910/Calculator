4a228a5369670f12399541d063917eba70bf2473
M1chael910 <mpmurphybusiness@gmail.com>
2024-01-30 13:46:41 -0600

    added modern UI

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function Calculate(operation) {
-  const firstBox = document.getElementById("first-box");
-  const secondBox = document.getElementById("second-box");
-  const resultLabel = document.getElementById("results-label");
   let x = Number(firstBox.value);
   let y = Number(secondBox.value);
   if (operation === 1) {
@@ -13,4 +10,13 @@ function Calculate(operation) {
   } else if (operation === 4) {
     resultLabel.innerHTML = String(x / y);
   }
+}
+
+
+
+
+function display() { 
+
+
+  
 }
